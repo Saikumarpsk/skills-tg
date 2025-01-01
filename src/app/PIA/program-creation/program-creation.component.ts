87e7342b6a964ef92f41366b60d4332be3f39aaa
saikumar <saikumar.patnaikuni@kellton.com>
2025-01-01 20:54:52 +0530

    first commit

---
+++ src/app/PIA/program-creation/program-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-program-creation',
+  templateUrl: './program-creation.component.html',
+  styleUrls: ['./program-creation.component.css']
+})
+export class ProgramCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { AdminComponent } from './admin';
+import { LoginComponent } from './login';
+import { AuthGuard } from './_helpers';
+import { Role } from './_models';
+import { ProfileComponent } from './profile/profile.component';
+import { PasswordSettingsComponent } from './password-settings/password-settings.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { ProgramCreationComponent } from './PIA/program-creation/program-creation.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'admin',
+        component: AdminComponent,
+        canActivate: [AuthGuard],
+        data: { roles: [Role.Admin] }
+    },
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        canActivate: [AuthGuard],
+        data: { roles: [Role.Admin,Role.User] }
+    },
+    {
+        path: 'password-settings',
+        component: PasswordSettingsComponent,
+        canActivate: [AuthGuard],
+        data: { roles: [Role.Admin,Role.User] }
+    },
+    {
+        path: 'user-registration',
+        component: UserRegistrationComponent,
+        canActivate: [AuthGuard],
+        data: { roles: [Role.Admin] }
+    },
+    {
+        path: 'program-creation',
+        component: ProgramCreationComponent,
+        canActivate: [AuthGuard],
+        data: { roles: [Role.Admin] }
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
